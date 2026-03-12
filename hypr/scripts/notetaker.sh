fd91ff3e3ea48d9ae71f51cbe0afa875651d850a
Devansh <devansh.jyoti8888@gmai.com>
2026-03-13 03:26:09 +0530

    Improved note taker scripts.

--- hypr/scripts/notetaker.sh
+++ hypr/scripts/notetaker.sh
@@ -2,13 +2,15 @@
 #thanks to youtube.com/@LeafshadeInteractive
 
 noteFilename="$HOME/Documents/notes/src/note-$(date +%Y-%m-%d).md"
+mkdir -p "$(dirname "$noteFilename")"
+#no error if existing, make parent directories as needed
 
-if [ ! -f $noteFilename ]; then
-    echo "# Notes for $(date +%Y-%m-%d)" > $noteFilename
+if [ ! -f "$noteFilename" ]; then
+    echo "# Notes for $(date +%Y-%m-%d)" > "$noteFilename"
 fi
 
 nvim -c "norm Go" \
     -c "norm Go## $(date +%H:%M)" \
     -c "norm G2o" \
     -c "norm zz" \
-    -c "startinsert" $noteFilename
+    -c "startinsert" "$noteFilename"
